ef425c528e8fdd23100eefdf008790326d0a06af
juabiDev <Mederos40@outlook.com>
2024-03-30 12:53:17 -0300

    feature: add header

---
+++ adopcion-perritos/src/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+
+export default function Header() {
+    return (
+      <header>
+        <img src="./public/images/logo3.jpg" alt="logo sitio" />
+        <nav>
+            <a href="">Inicio</a>
+            <a href="">Protectoras</a>
+            <a href="">Adopcion</a>
+            <a href="">¿Quienes Somos?</a>
+        </nav>
+    </header>
+    )
+}
