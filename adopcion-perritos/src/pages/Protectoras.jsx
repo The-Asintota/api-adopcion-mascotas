dffd6eb67d0c591606edadf0b4f7d7e5336556d0
anggifit <abonaldy@gmail.com>
2024-03-31 20:56:06 +0200

    changing homepage width

--- adopcion-perritos/src/pages/Protectoras.jsx
+++ adopcion-perritos/src/pages/Protectoras.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from '../components/Header/Header'
 
 const Protectoras = () => {
   return (
-    <div>
-      hello world!
-    </div>
+    <Header/>
   )
 }
 
