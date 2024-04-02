52aafb9ad5714ab42c393ba108f7c3939f2acb2a
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-04-02 10:01:13 -0500

    chore(backend): initial files and configuration

---
+++ backend/src/settings/environments/production.py
@@ -0,0 +1,44 @@
+import dj_database_url
+
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config("DEBUG", cast=bool)
+
+ALLOWED_HOSTS = [
+    config("CLIENT_HOST", cast=str),
+    config("SERVER_HOST", cast=str),
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{config('SERVER_HOST', cast=str)}",
+    f"https://{config('CLIENT_HOST', cast=str)}",
+]
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [f"https://{config('CLIENT_HOST', cast=str)}"]
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+DATABASES = {
+    "default": dj_database_url.config(
+        default=config("POSTGRE_DB_URL", cast=str),
+        engine="django.db.backends.postgresql",
+    ),
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = Path.joinpath(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
