56f5b188afbea33fc686c22902850a5a588b64e1
juabiDev <Mederos40@outlook.com>
2024-04-02 10:19:23 -0300

    feat: add list of animals

--- frontend/src/components/AdoptionSection/AdoptionSection.jsx
+++ frontend/src/components/AdoptionSection/AdoptionSection.jsx
@@ -2,28 +2,34 @@ import React, { useState } from "react";
 import PetRequest from "../Forms/PetRequest/PetRequest";
 import AdoptionCard from "./AdoptionCard";
 import './AdoptionSection.css'
+import { mappedAnimals } from "../../../utils/animals";
 
 const AdoptionSection = () => {
   const [showPetRequest, setShowPetRequest] = useState(false)
-
+  const [animals, setAnimals] = useState(mappedAnimals())
   function handlePetRequest() {
     setShowPetRequest(true)
   }
-
+  
+  
   return (
     <section className="adoption-section">
       <h2>Adopción</h2>
       <ul className="animal-list">
-        <AdoptionCard 
-        onClick={handlePetRequest}
-        urlImage="https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        petName="Tito"
-        age="10 Meses"
-        breed="De la vida"
-        size="Pequeño"
-        description="Juguetón y muy cariñoso"
-        observations="Vacunas al dia"
-        />
+        {animals.map(({ id, urlImage, petName, age, bread, size, description, observations }) => (
+          <AdoptionCard 
+            key={id}
+            onClick={handlePetRequest}
+            urlImage={urlImage}
+            petName={petName}
+            age={age}
+            breed={bread}
+            size={size}
+            description={description}
+            observations={observations}
+          />
+        )) }
+
       </ul>
 
       <PetRequest isActive={showPetRequest} onClose={() => setShowPetRequest(false)}/>
