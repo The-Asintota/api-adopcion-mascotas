29c42c72c36814b1440f42d4a290cbb9037cefe2
juabiDev <mederos40@outlook.com>
2024-04-04 17:08:11 -0300

    feat: add context for manage users

---
+++ frontend/src/context/admin.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+export const AdminContext = createContext()
+
+export const AdminProvider = ({ children }) => {
+    const [user, setUser] = useState("admin") // admin, shelter or null
+
+    return (
+        <AdminContext.Provider value={{ user, setUser }}>
+            {children}
+        </AdminContext.Provider>
+    )
+    
+}
