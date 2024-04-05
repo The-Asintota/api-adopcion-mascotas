c328a40baf3469a88e60d5b0f8a5c08e951e1f91
anggifit <abonaldy@gmail.com>
2024-04-05 17:12:02 +0200

    feat(frontend): Add backend database URL and configure proxy in Vite
    Includes the addition of the backend database URL to the project configuration. Additionally, it configures the proxy in Vite to accept requests to the backend server.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api/v1/shelter/": {
+        target: "https://api-adopcion-mascotas-dev-qprf.2.us-1.fl0.io",
+        changeOrigin: true,
+      },
+    }
+  }
 })
