40a3b727016c0df1739bbc6a1ef77801640f8879
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-04-05 19:55:00 -0500

    feat(backend): User and Shelter models added to Django admin panel

--- backend/src/apps/users/admin.py
+++ backend/src/apps/users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import User, Shelter
 
-# Register your models here.
+
+class UserAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the User model.
+    """
+
+    list_display = (
+        "uuid",
+        "email",
+        "password",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "date_joined",
+        "last_login",
+    )
+    search_fields = (
+        "uuid",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "date_joined",
+    )
+
+
+class ShelterAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the Shelter model.
+    """
+
+    list_display = (
+        "uuid",
+        "user",
+        "name",
+        "address",
+        "phone_number",
+        "responsible",
+        "logo_url",
+        "data_joined",
+    )
+    search_fields = (
+        "uuid",
+        "user",
+        "data_joined",
+    )
+
+
+admin.site.register(User, UserAdminPanel)
+admin.site.register(Shelter, ShelterAdminPanel)
