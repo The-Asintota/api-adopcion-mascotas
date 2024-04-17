bbb0b6b81d0ddaea9aa11d1a285723b9f8e3ba02
Carlos Aguirre <aguizandre.2803@gmail.com>
2024-04-17 15:08:53 -0500

    feat(backend): 'EmailsSent' model added to admin panel

--- backend/src/apps/emails/admin.py
+++ backend/src/apps/emails/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import EmailsSent
 
-# Register your models here.
+
+class EmailsSentAdminPanel(admin.ModelAdmin):
+    """
+    Admin panel configuration for the `EmailsSent` model.
+    """
+
+    list_display = (
+        "uuid",
+        "subject",
+        "message",
+        "addressee",
+        "additional_info",
+        "date_joined",
+    )
+    search_fields = ("addressee",)
+
+
+admin.site.register(EmailsSent, EmailsSentAdminPanel)
