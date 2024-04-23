ba9fcb1ba01baf3f8d66a14bc9e1393db6943f9a
juabiDev <Mederos40@outlook.com>
2024-04-23 17:16:56 -0300

    refactor: change name of custom hook

--- frontend/src/components/SearchAnimal/SearchAnimal.jsx
+++ frontend/src/components/SearchAnimal/SearchAnimal.jsx
@@ -1,10 +1,12 @@
+import useAnimals from "../../hooks/useAnimals";
 import "./SearchAnimal.css";
 
-export default function SearchAnimal ({ onChange }) {
+export default function SearchAnimal () {
+    const { handleSearch } = useAnimals();
 
     return (
         <section className="flex items-center justify-center mt-10 mb-5">
-            <form onSubmit={onChange} className="search-animal flex gap-5 "> 
+            <form onSubmit={handleSearch} className="search-animal flex gap-5 "> 
                 <input name="nombre" id="nombre" className="rounded-md text-gray-950 px-6" type="text" placeholder="Firulais" />
                 <select className="rounded-md bg-[var(--green)] px-2" name="tipo" id="tipo">
                     <option value="Tipo">Tipo</option>
@@ -19,8 +21,8 @@ export default function SearchAnimal ({ onChange }) {
                 </select>
                 <select className="rounded-md bg-[var(--green)] px-2" name="edad" id="edad">
                     <option value="Edad">Edad</option>
-                    <option value="1 año">1 año</option>
-                    <option value="2 años">2 años</option>
+                    <option value="1">1 año</option>
+                    <option value="2">2 años</option>
                 </select>
                 <input className="bg-[var(--green)] cursor-pointer w-full py-2 px-8 rounded-md shadow-lg font-semibold " 
                     type="submit" 
