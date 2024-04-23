c73ae564c99b57eef21353c32fe310ca22ca083d
juabiDev <Mederos40@outlook.com>
2024-04-23 17:09:18 -0300

    refactor: change name for custom hook and use Animals context

---
+++ frontend/src/hooks/useAnimals.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { AnimalsContext } from "../context/animals";
+
+export default function useAnimals() {
+    const { animals, handleSearch } = useContext(AnimalsContext);
+
+    const getAnimalById = ({ id }) => {
+        const animal = animals?.find(animal => animal.id === id);
+        return animal;
+    }
+
+    return { animals, getAnimalById, handleSearch }
+}
