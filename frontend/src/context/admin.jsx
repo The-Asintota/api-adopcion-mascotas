d5b3d0e65d06ef4ce8161ad5bae77176447ffd0a
juabiDev <Mederos40@outlook.com>
2024-04-25 14:04:58 -0300

    feat: add functions for log out user,  check if user is login and add user data in session storage

--- frontend/src/context/admin.jsx
+++ frontend/src/context/admin.jsx
@@ -4,18 +4,39 @@ import { USERS } from "../../utils/constants";
 export const AdminContext = createContext()
 
 export const AdminProvider = ({ children }) => {
-    const [user, setUser] = useState(null) 
-    
-    const authenticateUser = (role) => {
+    const [user, setUser] = useState(() => {
+        const role = sessionStorage.getItem("role")
+        const token = sessionStorage.getItem("token")
+
+        return role && token ? { role, token } : null
+    }) 
+
+    const authenticateUser = ({ role, token }) => {
         if (role === USERS.ADMIN || role === USERS.SHELTER) {
-            setUser({role})
+            setUser({ role, token })
+            sessionStorage.setItem("role", role)
+            sessionStorage.setItem("token", token)
         } else {
             setUser(null)
         }
     }
 
+    const logOutAdmin = () => {
+        setUser({
+            role: null,
+            token: null
+        })
+
+        sessionStorage.removeItem("role")
+        sessionStorage.removeItem("token")
+    }
+
+    const isLogged = () => {
+        return user?.role !== null && user?.token !== null
+    }
+
     return (
-        <AdminContext.Provider value={{ user, authenticateUser }}>
+        <AdminContext.Provider value={{ user, authenticateUser, logOutAdmin, isLogged }}>
             {children}
         </AdminContext.Provider>
     )
